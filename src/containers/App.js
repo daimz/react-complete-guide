ce172383c7ccac397633d0e23d739aa84961c594
Damien Metcalf <daimz06@gmail.com>
2021-03-12 09:45:12 +1100

    lifeCycleHooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: 'asfa1', name: 'Max', age: 28 },
@@ -12,6 +17,19 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentWillMount() {
+    console.log('[App.js] componentWillMount');
+  }
+  
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
   }
 
   nameChangedHandler = ( event, id ) => {
@@ -46,9 +64,9 @@ class App extends Component {
   }
 
   render () {
+    console.log('[App.js] render');
     let persons = null;
     
-
     if ( this.state.showPersons ) {
       persons = <Persons 
                   persons={this.state.persons}
@@ -56,11 +74,10 @@ class App extends Component {
                   changed={this.nameChangedHandler} />;
     }
 
-    
-
     return (
       <div className={classes.App}>
         <Cockpit 
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons} 
           clicked={this.togglePersonsHandler}/>
