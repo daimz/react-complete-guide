217d3c62392b0e4e8660b4cc49383ea613aa49e1
Damien Metcalf <daimz06@gmail.com>
2021-03-12 16:23:36 +1100

    Class component life cycle hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
+  // static getDeriversStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: 'Snapshot'};
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot)
+  }
+
   render() {
     console.log('[Persons.js] rendering...');
     return this.props.persons.map( ( person, index ) => {
       return <Person
+          key={person.id}
           click={() => this.props.clicked( index )}
           name={person.name}
           age={person.age}
