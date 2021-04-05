f1867a26a809b053dfe5973e82643f3bfaad4367
Damien Metcalf <daimz06@gmail.com>
2021-04-06 07:23:40 +1000

    convetr to PureComponent

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
   // static getDeriversStateFromProps(props, state) {
   //   console.log('[Persons.js] getDerivedStateFromProps');
   //   return state;
   // }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[Persons.js] shouldComponentUpdate');
-    return true;
-  }
+  // Use with Component not PureComponent
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[Persons.js] shouldComponentUpdate');
+  //   if (
+  //     nextProps.persons !== this.props.persons || 
+  //     nextProps.changed !== this.props.changed || 
+  //     nextProps.clicked !== this.props.clicked ) {
+  //     return true
+  //   } else {
+  //     return false;
+  //   }
+  // }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
     console.log('[Persons.js] getSnapshotBeforeUpdate');
@@ -26,10 +34,10 @@ class Persons extends Component {
     console.log('[Persons.js] rendering...');
     return this.props.persons.map( ( person, index ) => {
       return <Person
-          key={person.id}
           click={() => this.props.clicked( index )}
           name={person.name}
           age={person.age}
+          key={person.id}
           changed={( event ) => this.props.changed( event, person.id )} />
     });
   };
